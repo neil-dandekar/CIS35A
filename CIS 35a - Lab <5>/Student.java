81875280bcd8047e300d8c07f3ca727e18ba62f0
Neil Dandekar <neildandekar10@gmail.com>
2023-03-07 11:14:42 -0800

    Student class - implemented not tested

--- CIS 35a - Lab <5>/Student.java
+++ CIS 35a - Lab <5>/Student.java
@@ -1,13 +1,46 @@
 class Student {
 
-    private int SID;
+    private int SID; // Student ID
 
-    private int scores[] = new int[5];
+    private int scores[] = new int[5]; // Student Scores
 
-    //write public get and set methods for
 
-    //SID and scores
-
+    // METHODS
+        // GETTERS
+    public int getSID() {
+        return SID;
+    }
+    public int[] getScores() {
+        return scores;
+    }
+    public int getScore(int index) {
+        return scores[index];
+    }
+        // SETTERS
+    public void setSID(int sID) {
+        SID = sID;
+    }
+    public void setScores(int[] scores) {
+        this.scores = scores;
+    }
+    public void setScore(int index, int score) {
+        this.scores[index] = score;
+    }
+    
     //add methods to print values of instance variables.
-
+    public void printSID() {
+        System.out.printf("%d ", SID);
+    }
+    public void printScore(int index) {
+        System.out.printf("%d ", scores[index]);
+    }
+    public void printScores() {
+        for (int i = 0; i < scores.length; i++) {
+            this.printScore(i);
+        }
+    }
+    public void print() {
+        printSID();
+        printScores();
+    }
 }
